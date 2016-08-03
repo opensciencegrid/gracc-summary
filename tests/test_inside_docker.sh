8042ba67626a4108f9e832e628d42595ed4f9da0
Derek Weitzel <djw8605@gmail.com>
2016-08-03 14:37:00 -0500

    Debug java locations

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -84,6 +84,9 @@ journalctl -u graccreq.service --no-pager -n 100
 journalctl -u logstash.service --no-pager -n 100
 cat /var/log/logstash/*
 
+ls -l /usr/bin/java
+namei /usr/bin/java
+
 journalctl -u graccsumperiodic.service --no-pager
 
 exit $unittest_exit
