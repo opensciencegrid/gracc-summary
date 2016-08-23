77b56e310f0778201d4d1c063a2ebbc622112c02
Derek Weitzel <djw8605@gmail.com>
2016-08-23 14:43:47 -0500

    Add default argument and bump version.

--- config/gracc-summary.spec
+++ config/gracc-summary.spec
@@ -1,5 +1,5 @@
 Name:           gracc-summary
-Version:        1.1
+Version:        1.2
 Release:        1%{?dist}
 Summary:        GRACC Summary Agents
 
@@ -62,6 +62,9 @@ install -m 0744 config/graccsumperiodic.service $RPM_BUILD_ROOT/%{_unitdir}/
 
 
 %changelog
+* Tue Aug 23 2016 Derek Weitzel <dweitzel@cse.unl.edu> 1.2-1
+- Adding the summarizer command line
+
 * Tue Aug 23 2016 Derek Weitzel <dweitzel@cse.unl.edu> 1.1-1
 - Adding the summarizer command line
 
--- src/graccsum/summarize.py
+++ src/graccsum/summarize.py
@@ -18,7 +18,7 @@ def main():
     parser.add_argument('--exchange', dest='exchange', help="Exchange to send summarize request", default="gracc.osg.requests")
     parser.add_argument('--routing_key', dest='routing_key', help="Routing key to use for summarize request", default="gracc.osg.requests")
     parser.add_argument('--host', dest='host', help="Host of AMQP broker", default="localhost")
-    parser.add_argument('--vhost', dest='vhost', help="Virtual host for the AMQP server")
+    parser.add_argument('--vhost', dest='vhost', help="Virtual host for the AMQP server", default="/")
     parser.add_argument('--username', dest='username', help="Username for AMQP broker", default="guest")
     parser.add_argument('--password', dest='password', help="Password for AMQP broker", default="guest")
     
