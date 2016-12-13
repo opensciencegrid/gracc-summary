5d7e7d49184af8342b80d8c13f4bd7337165ffb8
Derek Weitzel <djw8605@gmail.com>
2016-12-13 10:00:01 -0600

    Debugging failure in timer

--- tests/test_inside_docker.sh
+++ tests/test_inside_docker.sh
@@ -66,8 +66,15 @@ bash -x ./import.sh
 popd
 
 # Start the gracc periodic summarizer after data has been imported
+set +e
 systemctl start graccsumperiodic.timer
+exit_code=$?
+systemctl status graccsumperiodic.timer
 systemctl list-timers --all
+if [ "$exit_code" -eq "1" ]; then
+  exit $exit_code
+fi
+set -e
 sleep 65
 journalctl -u graccsumperiodic.service --no-pager
 
